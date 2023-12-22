063f7a59a97a1404ce0f56565eca0dc79a5e44d7
abhijeetdabhi <143803214+abhijeetdabhi@users.noreply.github.com>
2023-12-22 16:46:34 +0530

    Update delete-note.js

--- delete-note.js
+++ delete-note.js
@@ -18,6 +18,7 @@ function deleteNote(index) {
 
 let popUp = document.getElementById('popUp');
 let close = document.getElementById('close');
+let body = document.getElementById('body');
 
 let currentDeleteIndex;
 
@@ -25,14 +26,44 @@ function showPopUp(index){
     currentDeleteIndex = index;
     popUp.style.opacity = '10';
     popUp.style.visibility = 'visible';
+    body.style.overflow = 'hidden';
+    
+
+    let inputTitle = document.getElementById('addTitle');
+    let textarea = document.getElementById('addText');
+    let addBtn = document.getElementById('addBtn');
+
+    inputTitle.disabled = true;
+    textarea.disabled = true;
+    addBtn.disabled = true;
+
+    let backgroundField = document.getElementsByClassName('background-field');
+    for(let i = 0; i < backgroundField.length; i++){
+        backgroundField[i].disabled = true;
+    }
 }
 
 function closePop(){
     popUp.style.opacity = '0';
     popUp.style.visibility = 'hidden';
+    body.style.overflow = 'scroll';
+
+    let inputTitle = document.getElementById('addTitle');
+    let textarea = document.getElementById('addText');
+    let addBtn = document.getElementById('addBtn');
+
+    inputTitle.disabled = false;
+    textarea.disabled = false;
+    addBtn.disabled = false;
+
+    let backgroundField = document.getElementsByClassName('background-field');
+    for(let i = 0; i < backgroundField.length; i++){
+        backgroundField[i].disabled = false;
+    }
 }
 
 function deleteItem(){
     deleteNote(currentDeleteIndex);
+    closePop();
 }
 
